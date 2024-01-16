ae8cb4abad6532a40ddc402cf45f0a02b73954a4
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-01-16 17:22:43 +0100

    Fresh secure start.

--- README.md
+++ README.md
@@ -1,1 +1,173 @@
 # repository_recipes_backend
+
+# repository-backend
+
+`Recipe Frontend Repository url` : [Frontend Repository]()
+
+## Table of Contents
+1. [Project Goals](#project-goals)
+2. [User Stories](#user-stories)
+3. [API Endpoints](#api-endpoints)
+4. [Technologies Used](#technologies-used)
+5. [Dependencies](#dependencies)
+6. [Building out the Backend](#building-out-the-backend)
+7. [Testing](#testing)
+8. [Database Management: ElephantSQL PostgreSQL](#database-management-elephantsql-postgresql)
+9. [Deployment](#deployment)
+10. [Credits & Tutorials](#credits-&-tutorials)
+
+----------
+
+## Project Goals
+
+
+[Back to top](#table-of-contents)
+
+----------
+
+## User Stories
+
+
+[Back to top](#table-of-contents)
+
+----------
+
+## API Endpoints
+
+Using router to centralize api endpoints. 
+
+
+
+[Back to top](#table-of-contents)
+
+----------
+
+## Technologies Used
+
+Django: 
+A high-level Python web framework that encourages rapid development and clean, pragmatic design
+
+Django REST Framework: 
+A powerful and flexible toolkit for building Web APIs in Django
+
+PostgreSQL: 
+Open source relational database with an emphasis on extensibility and standards compliance
+
+Django Allauth: 
+Provides authentication mechanisms via traditional username/email and password login or through various third-party providers
+
+Django CORS Headers: 
+Django application for handling the server headers required for Cross-Origin Resource Sharing CORS
+
+Dj-Rest-Auth: 
+RESTful API views for authentication, using Django REST Framework
+
+Cloudinary: 
+Cloud-based service that provides a solution for managing web and mobile media assets images and videos
+
+Pillow: 
+Python Imaging Library adds image processing capabilities to python 
+
+
+
+[Back to top](#table-of-contents)
+
+----------
+
+## Dependencies
+
+- `asgiref==3.7.2`: ASGI interface specification to allow Django to run on ASGI web servers.
+
+- `dj-database-url==2.1.0`: Utility to help you configure your database using a URL.
+
+- `Django==4.2.7`: The web framework for building web applications.
+
+- `django-cors-headers==4.3.1`: Handles Cross-Origin Resource Sharing (CORS) for Django applications.
+
+- `dj-rest-auth==2.2.4`: Provides a set of REST API endpoints for authentication and registration.
+
+- `django-allauth==0.47.0`: Integrated set of Django applications addressing authentication, registration, account management, and third-party (social) account authentication.
+
+- `djangorestframework==3.14.0`: Toolkit for building Web APIs in Django.
+
+- `oauthlib==3.2.0`: A generic, spec-compliant implementation of OAuth for managing secure tokens.
+
+- `gunicorn==21.2.0`: WSGI HTTP Server for UNIX, for serving Python web applications.
+
+- `psycopg2-binary==2.9.9`: PostgreSQL database adapter for Python.
+
+- `python-decouple==3.8`: Simplifies the usage of configuration variables or secrets in your application.
+
+- `pytz==2023.3.post1`: Cross-platform timezone calculations using Python.
+
+- `sqlparse==0.4.4`: A non-validating SQL parser module for Python to format SQL strings.
+
+- `Pillow==10.1.0`: The Python Imaging Library adds image processing capabilities.
+
+- `cloudinary==1.37.0`: Cloud service for managing web and mobile media assets.
+
+- `dj3-cloudinary-storage==0.0.6`: Django storage backend for Cloudinary.
+
+- `urllib3`: A powerful HTTP client for Python.
+
+- `requests==2.31.0`: Elegant and simple HTTP library for Python.
+
+- `django-filter==23.5`: A library to filter querysets based on user selections.
+
+
+
+
+[Back to top](#table-of-contents)
+
+
+
+
+----------
+
+## Building out the Backend
+
+
+[Back to top](#table-of-contents)
+
+----------
+
+## Testing
+
+### Postman testing Authentication endpoints 
+
+<p>
+ <img src="/testing/images/postman-signup.png" alt="image 1" width="350"/>
+ <img src="/testing/images/postman-lg.png" alt="image 2" width="350"/> 
+ </p>
+
+
+
+
+[Back to top](#table-of-contents)
+
+----------
+
+## Database Management: ElephantSQL PostgreSQL
+
+
+[Back to top](#table-of-contents)
+
+----------
+
+## Deployment
+
+
+[Back to top](#table-of-contents)
+
+----------
+
+## Credits & Tutorials
+
+
+### README Management
+- https://stackedit.io
+
+
+[Back to top](#table-of-contents)
+
+----------
---
+++ backend/settings.py
@@ -0,0 +1,185 @@
+import re
+import os
+from pathlib import Path
+from decouple import config
+import cloudinary
+import cloudinary_storage
+import dj_database_url
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = [config('ALLOWED_HOST')]
+
+
+CSRF_TRUSTED_ORIGINS = [
+    
+    config('CSRF_TRUSTED_ORIGIN_1'),
+    config('CSRF_TRUSTED_ORIGIN_2'),
+    # remove last one before production
+    config('CSRF_TRUSTED_ORIGIN_3'),
+]
+
+CORS_ALLOWED_ORIGINS = [    
+    config('CORS_ALLOWED_ORIGIN_1'),
+    config('CORS_ALLOWED_ORIGIN_2'),
+    # remove last one before production
+    config('CORS_ALLOWED_ORIGIN_3'),
+]
+
+
+# CSRF & CORS
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = False
+
+
+# site id 
+SITE_ID = 1
+
+
+# security headers 
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+}
+
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'users.serializers.CurrentUserSerializer'
+}
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',    
+    'corsheaders',
+    
+    ]
+
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+
+#DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
